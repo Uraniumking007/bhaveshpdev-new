5c8dd9c640375f5aea60fe35741112f00021bf6c
UraniumKing <96092183+Uraniumking007@users.noreply.github.com>
2023-03-18 21:35:24 +0530

    Added New Array and Types

---
+++ src/app/utils/projectArr.ts
@@ -0,0 +1,89 @@
+export interface ProjectsDetails {
+  githubLink: string;
+  title: string;
+  description: string;
+  imgLink: string;
+  previewLink: string;
+  warning: string;
+}
+
+export const projectsArr = [
+  {
+    githubLink: 'https://github.com/Uraniumking007/todo-app-new',
+    title: 'Todo App',
+    description:
+      "This is a simple web application for managing your todo list. You can add, edit, and delete tasks, and mark them as completed when you're done.",
+    imgLink:
+      'https://cdn.discordapp.com/attachments/1079592365821526078/1081903624394911924/Fqc6rWuWcAEtQL7.png',
+    previewLink: 'https://todo.bhaveshp.dev/',
+    warning: '',
+  },
+  {
+    githubLink: 'https://github.com/Uraniumking007/Expense-Manager',
+    title: 'Expense Manager',
+    description:
+      'A simple Expense Manager which stores data in local storage of your browser.',
+    imgLink:
+      'https://cdn.discordapp.com/attachments/1079592365821526078/1079592439528042627/image.png',
+    previewLink: 'https://expensemanager.bhaveshp.dev/',
+    warning: '',
+  },
+  {
+    githubLink: 'https://github.com/Uraniumking007/digital-clock',
+    title: 'Digital Clock',
+    description: 'A simple Digital Clock',
+    imgLink:
+      'https://cdn.discordapp.com/attachments/1056086845693370439/1065513304988078090/image.png',
+    previewLink: 'https://digitalclock.bhaveshp.dev/',
+    warning: '',
+  },
+  {
+    githubLink: 'https://github.com/Uraniumking007/stckme-clone',
+    title: 'Stackme Clone',
+    description: 'This is a clone of Stackme made only html and css.',
+    imgLink:
+      'https://cdn.discordapp.com/attachments/1079592365821526078/1079609336541757520/image.png',
+    previewLink: 'https://deft-fudge-10fa95.netlify.app/',
+    warning: 'Only Optimized for Mobile',
+  },
+  {
+    githubLink: 'https://github.com/Uraniumking007/linktree-design-clone',
+    title: 'Linktree Design Clone',
+    description: 'This is a clone of linktree made only html and css.',
+    imgLink:
+      'https://cdn.discordapp.com/attachments/1056086845693370439/1056105527240818768/ezgif.com-gif-maker_1.gif',
+    previewLink: 'https://exquisite-sprite-7700d4.netlify.app/',
+    warning: '',
+  },
+  {
+    githubLink: 'https://github.com/Uraniumking007/stopwatch',
+    title: 'Stopwatch',
+    description:
+      'This is a simple stopwatch app built with JavaScript. Read about how to build it on my blog.',
+    imgLink:
+      'https://cdn.discordapp.com/attachments/1057923395162996796/1059046443383529482/Screenshot_2022-12-30_225610.png',
+    previewLink: 'https://stopwatch.bhaveshp.dev/',
+    warning: '',
+  },
+  {
+    githubLink: 'https://github.com/Uraniumking007/Temperature-Converter',
+    title: 'Temperature Converter',
+    description:
+      'Temperature-Converter is a simple temperature converter which convert temperature in other temperature unit as soon as value is typed.',
+    imgLink:
+      'https://cdn.discordapp.com/attachments/1056086845693370439/1059461203824496640/image.png',
+    previewLink: 'https://tempconvert.bhaveshp.dev/',
+    warning: '',
+  },
+
+  {
+    githubLink:
+      'https://github.com/Uraniumking007/Coming-Soon-Page-for-Website',
+    title: 'Coming Soon for Website',
+    description: 'A Simple animated coming soon Page for website',
+    imgLink:
+      'https://cdn.discordapp.com/attachments/1056086845693370439/1060924534821236778/image.png',
+    previewLink: 'https://coming-soon.bhaveshp.dev/',
+    warning: '',
+  },
+];
