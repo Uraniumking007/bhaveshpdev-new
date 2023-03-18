80d3aec6c5cd047359d4aa924e5e3d02876c2804
UraniumKing <96092183+Uraniumking007@users.noreply.github.com>
2023-03-19 00:42:21 +0530

    Added Remote Patterns

--- next.config.js
+++ next.config.js
@@ -1,8 +1,26 @@
+const { hostname } = require('os');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    remotePatterns: [
+      // {
+      //   protocol: 'https',
+      //   hostname: 'example.com',
+      //   port: '',
+      //   pathname: '/account123/**',
+      // },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        port: '',
+        pathname: '/attachments/**/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
