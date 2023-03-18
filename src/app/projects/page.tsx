aa2d174520c0a6f5aed747d5397803f61bfe54c3
UraniumKing <96092183+Uraniumking007@users.noreply.github.com>
2023-03-19 00:43:28 +0530

    Added New Page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProjectCards from '../components/Cards';
+
+const projects = () => {
+  return (
+    <div className='flex m-8 flex-wrap justify-evenly'>
+      <ProjectCards />
+    </div>
+  );
+};
+
+export default projects;
