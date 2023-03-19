11253e8270a96750fd1c84eaf2024fabe8ed0095
Uraniumking007 <96092183+Uraniumking007@users.noreply.github.com>
2023-03-19 09:50:26 +0530

    Responsive

--- src/app/components/Cards.tsx
+++ src/app/components/Cards.tsx
@@ -8,30 +8,33 @@ const Card = (project: ProjectsDetails) => {
     <div className='m-4'>
       <div
         data-theme='business'
-        className='card w-96 bg-base-100 shadow-xl h-96 rounded-3xl hover:drop-shadow-4xl hover:scale-105 '
+        className='card md:w-96 w-64 md:text-lg text-sm bg-base-100 shadow-xl h-96 rounded-3xl hover:drop-shadow-4xl hover:scale-105 '
       >
-        <figure>
+        <figure className='relative h-96'>
           <Image
             src={project.imgLink}
             alt={project.title}
-            width={1280}
-            height={720}
+            // width={1280}
+            // height={720}
+            fill
           />
         </figure>
-        <div className='card-body'>
-          <h2 className='card-title'>{project.title}</h2>
+        <div className='card-body p-4 md:p-8'>
+          <h2 className='card-title font-semibold '>{project.title}</h2>
           <p>{project.description}</p>
           <div className=' mt-2 flex justify-evenly'>
             <Link href={project.previewLink}>
               <div className='card-actions justify-end'>
-                <button className='btn btn-primary rounded-2xl '>
+                <button className='md:btn btn-primary btn-sm rounded-lg md:rounded-2xl '>
                   Preview
                 </button>
               </div>
             </Link>
             <Link href={project.githubLink}>
               <div className='card-actions justify-end'>
-                <button className='btn btn-primary rounded-2xl'>GitHub</button>
+                <button className='md:btn btn-primary btn-sm rounded-lg md:rounded-2xl'>
+                  GitHub
+                </button>
               </div>
             </Link>
           </div>
