ecd16342d99316e2658ac869cab13810b6470362
Uraniumking007 <96092183+Uraniumking007@users.noreply.github.com>
2023-03-19 09:50:14 +0530

    Added Small Introduction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 import CollapseProjects from './components/Collapse';
+import Introduction from './components/introduction';
 
 export default function Home() {
   return (
     <main className='flex justify-center p-2'>
       <div className='p-25'>
+        <Introduction />
         <CollapseProjects />
       </div>
     </main>
