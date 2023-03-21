83ba14c3cbe278b8ab1d4b28bbffaacb362b12f0
UraniumKing <96092183+Uraniumking007@users.noreply.github.com>
2023-03-21 17:22:39 +0530

    removed collapses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,9 @@ import Introduction from './components/introduction';
 
 export default function Home() {
   return (
-    <main className='flex justify-center p-2'>
-      <div className='p-25'>
-        <Introduction />
-        <CollapseProjects />
-      </div>
+    <main className='flex justify-center items-center select-none h-[calc(100%-4rem)] w-full'>
+      <Introduction />
+      {/* <CollapseProjects /> */}
     </main>
   );
 }
