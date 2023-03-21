0eacf61a38fe5a0cd324ec8879770fd573da1efa
UraniumKing <96092183+Uraniumking007@users.noreply.github.com>
2023-03-21 17:22:10 +0530

    Removed Collapse

--- src/app/components/introduction.tsx
+++ src/app/components/introduction.tsx
@@ -11,10 +11,11 @@ const Introduction = () => {
           </div>
         </div>
         <div className='text-sm tracking-normal lg:text-xl text-center items-center'>
-          Hi! I&apos;m Bhavesh, and I&apos;m a computer engineering student who
-          enjoys using JavaScript, TypeScript, Next.js, and Tailwind CSS. I am
-          interested about creating dynamic, responsive web applications that
-          provide an excellent user experience.
+          Hi! I&apos;m <span className=' text-yellow-400'>Bhavesh</span>, and
+          I&apos;m a computer engineering student who enjoys using JavaScript,
+          TypeScript, Next.js, and Tailwind CSS. I am interested about creating
+          dynamic, responsive web applications that provide an excellent user
+          experience.
           <div className='flex justify-center items-center'>
             <div className='text-lg'>Contact Me : </div>
             <div className='m-1 active:scale-75 ease-in-out duration-500 sm:m-2'>
