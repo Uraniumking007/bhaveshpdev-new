8b6aed1766c5c0c5a6bd13540053241158fd67a0
UraniumKing <96092183+Uraniumking007@users.noreply.github.com>
2023-03-23 14:52:53 +0530

    Added Skills Page

---
+++ src/app/skills/page.tsx
@@ -0,0 +1,11 @@
+import SkillIcons from '../components/SkillIcons';
+
+const page = () => {
+  return (
+    <div className='flex justify-center items-center select-none h-[calc(100%-4rem)] w-full'>
+      <SkillIcons />
+    </div>
+  );
+};
+
+export default page;
