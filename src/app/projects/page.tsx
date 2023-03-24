cfa0cff7e87b0cfe026f9b014ecac238db75e0a2
UraniumKing <96092183+Uraniumking007@users.noreply.github.com>
2023-03-24 11:12:45 +0530

    Added Title

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ProjectCards from '../components/Cards';
+import Head from 'next/head';
 
 const projects = () => {
   return (
-    <div className='flex select-none h-[calc(100%-4rem)] w-full'>
-      <div className='flex m-20 flex-wrap justify-evenly'>
-        <ProjectCards />
-      </div>
-    </div>
+    <>
+      <Head>
+        <title>Bhavesh Patil - Projects</title>
+      </Head>
+      <main>
+        <div className='flex select-none h-[calc(100%-4rem)] w-full'>
+          <div className='flex m-20 flex-wrap justify-evenly'>
+            <ProjectCards />
+          </div>
+        </div>
+      </main>
+    </>
   );
 };
 
