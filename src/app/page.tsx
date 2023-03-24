974fc949a27a5177cd523e6326e12df2bf044d8c
UraniumKing <96092183+Uraniumking007@users.noreply.github.com>
2023-03-24 11:09:41 +0530

    Added New Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 'use client';
-import CollapseProjects from './components/Collapse';
+import Head from 'next/head';
 import Introduction from './components/introduction';
 
 export default function Home() {
   return (
-    <main className='flex justify-center items-center select-none h-[calc(100%-4rem)] w-full'>
-      <Introduction />
-      {/* <CollapseProjects /> */}
-    </main>
+    <>
+      <Head>
+        <title>Bhavesh Patil - Home</title>
+      </Head>
+      <main className='flex justify-center items-center select-none h-[calc(100%-4rem)] w-full'>
+        <Introduction />
+        {/* <CollapseProjects /> */}
+      </main>
+    </>
   );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,8 +3,10 @@ import ProjectCards from '../components/Cards';
 
 const projects = () => {
   return (
-    <div className='flex m-8 flex-wrap justify-evenly'>
-      <ProjectCards />
+    <div className='flex select-none h-[calc(100%-4rem)] w-full'>
+      <div className='flex m-20 flex-wrap justify-evenly'>
+        <ProjectCards />
+      </div>
     </div>
   );
 };
