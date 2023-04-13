334a6168c23ad926cb51df1bdedfaf3d42953f47
Uraniumking007 <96092183+Uraniumking007@users.noreply.github.com>
2023-04-13 22:06:56 +0530

    Fixed NavBar Buttons

--- src/app/components/BrandNewHeader.tsx
+++ src/app/components/BrandNewHeader.tsx
@@ -1,128 +1,124 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
 import {
   Navbar,
   MobileNav,
   Typography,
   IconButton,
   Button,
-} from '@material-tailwind/react';
-import Image from 'next/image';
-import Link from 'next/link';
+} from "@material-tailwind/react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function BrandNewHeader() {
   const [openNav, setOpenNav] = React.useState(false);
 
   React.useEffect(() => {
     window.addEventListener(
-      'resize',
+      "resize",
       () => window.innerWidth >= 960 && setOpenNav(false)
     );
   }, []);
 
   const navList = (
-    <ul className='mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6'>
-      <Button className='m-0 p-0 capitalize md:p-1 bg-transparent text-base-content shadow-none hover:shadow-none'>
-        <Typography
-          as='li'
-          variant='small'
-          color='blue-gray'
-          className='p-1 font-normal'
-        >
-          <Link href='/' className='flex text-base-content items-center'>
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Link href="/" className="flex text-base-content items-center">
+        <Button className="m-0 p-0 capitalize md:p-1 bg-transparent text-base-content shadow-none hover:shadow-none">
+          <Typography
+            as="li"
+            variant="small"
+            color="blue-gray"
+            className="p-1 font-normal"
+          >
             Home
-          </Link>
-        </Typography>
-      </Button>
-      <Button className='m-0 p-0 capitalize md:p-1 bg-transparent text-base-content shadow-none hover:shadow-none'>
-        <Typography
-          as='li'
-          variant='small'
-          color='blue-gray'
-          className='p-1 font-normal'
-        >
-          <Link
-            href='/projects'
-            className='flex items-center text-base-content'
+          </Typography>
+        </Button>
+      </Link>
+      <Link href="/projects" className="flex items-center text-base-content">
+        <Button className="m-0 p-0 capitalize md:p-1 bg-transparent text-base-content shadow-none hover:shadow-none">
+          <Typography
+            as="li"
+            variant="small"
+            color="blue-gray"
+            className="p-1 font-normal"
           >
             Projects
-          </Link>
-        </Typography>
-      </Button>
-      <Button className='m-0 p-0 capitalize bg-transparent text-base-content shadow-none hover:shadow-none'>
-        <Typography
-          as='li'
-          variant='small'
-          color='blue-gray'
-          className='p-1 font-normal'
-        >
-          <Link href='/skills' className='flex items-center text-base-content'>
+          </Typography>
+        </Button>
+      </Link>
+      <Link href="/skills" className="flex items-center text-base-content">
+        <Button className="m-0 p-0 capitalize bg-transparent text-base-content shadow-none hover:shadow-none">
+          <Typography
+            as="li"
+            variant="small"
+            color="blue-gray"
+            className="p-1 font-normal"
+          >
             Skills
-          </Link>
-        </Typography>
-      </Button>
-      <Button className='m-0 p-0 capitalize md:p-1 bg-transparent text-base-content shadow-none hover:shadow-none'>
-        <Typography
-          as='li'
-          variant='small'
-          color='blue-gray'
-          className='p-1 font-normal'
-        >
-          <Link href='/about' className='flex items-center text-base-content'>
+          </Typography>
+        </Button>
+      </Link>
+      <Link href="/about" className="flex items-center text-base-content">
+        <Button className="m-0 p-0 capitalize md:p-1 bg-transparent text-base-content shadow-none hover:shadow-none">
+          <Typography
+            as="li"
+            variant="small"
+            className="p-1 text-base-content font-normal"
+          >
             About
-          </Link>
-        </Typography>
-      </Button>
+          </Typography>
+        </Button>
+      </Link>
     </ul>
   );
 
   return (
     <>
-      <Navbar className='sticky inset-0 z-10 h-max max-w-full bg-base-300 border-none shadow-lg shadow-black/40  rounded-none py-2 px-4 lg:px-8 lg:py-4'>
-        <div className='flex items-center justify-between text-base-content'>
-          <div className='btn btn-ghost'>
+      <Navbar className="sticky inset-0 z-10 h-max max-w-full bg-base-300 border-none shadow-lg shadow-black/40  rounded-none py-2 px-4 lg:px-8 lg:py-4">
+        <div className="flex items-center justify-between text-base-content">
+          <div className="btn btn-ghost">
             <Image
-              src={'/BhaveshPatilText.png'}
-              alt={''}
+              src={"/BhaveshPatilText.png"}
+              alt={""}
               width={250}
               height={30}
             />
           </div>
-          <div className='flex items-center gap-4'>
-            <div className='mr-4 hidden lg:block'>{navList}</div>
+          <div className="flex items-center gap-4">
+            <div className="mr-4 hidden lg:block">{navList}</div>
             <IconButton
-              variant='text'
-              className='ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden'
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
               ripple={false}
               onClick={() => setOpenNav(!openNav)}
             >
               {openNav ? (
                 <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  className='h-6 w-6'
-                  viewBox='0 0 24 24'
-                  stroke='currentColor'
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
                   strokeWidth={2}
                 >
                   <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M6 18L18 6M6 6l12 12'
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
                   />
                 </svg>
               ) : (
                 <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  className='h-6 w-6'
-                  fill='none'
-                  stroke='currentColor'
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
                   strokeWidth={2}
                 >
                   <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M4 6h16M4 12h16M4 18h16'
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
                   />
                 </svg>
               )}
--- src/app/components/NewHeader.tsx
+++ src/app/components/NewHeader.tsx
@@ -1,7 +1,7 @@
-'use client';
-import Image from 'next/image';
-import Link from 'next/link';
-import React, { useState } from 'react';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
 
 const NewHeader = () => {
   const [open, setOpen] = useState(false);
@@ -11,53 +11,53 @@ const NewHeader = () => {
     }, 160);
   };
   return (
-    <div className='fixed bg-base w-full h-20 shadow-xl z-[100]'>
-      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+    <div className="fixed bg-base w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <div
-          className='md:hidden active:scale-75 ease-in-out duration-150'
+          className="md:hidden active:scale-75 ease-in-out duration-150"
           onClick={toggleMenu}
         >
           <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='h-5 w-5'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
           >
             <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              d='M4 6h16M4 12h16M4 18h7'
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h7"
             />
           </svg>
         </div>
-        <a className='hover:bg-base-content/10 p-2 rounded-lg active:scale-90 duration-150 ease-in-out normal-case text-xl '>
-          <div className='relative lg:w-96 md:w-64 w-40  lg:h-12 md:h-8 h-8'>
-            <Image src={'/BhaveshPatilText.png'} fill alt={''} />
+        <a className="hover:bg-base-content/10 p-2 rounded-lg active:scale-90 duration-150 ease-in-out normal-case text-xl ">
+          <div className="relative lg:w-96 md:w-64 w-40  lg:h-12 md:h-8 h-8">
+            <Image src={"/BhaveshPatilText.png"} fill alt={""} />
           </div>
         </a>
         <div>
-          <div className='flex md:hidden pr-2 active:scale-75 ease-in-out duration-500'>
+          <div className="flex md:hidden pr-2 active:scale-75 ease-in-out duration-500">
             <Image
-              src={'/icons/Social/github-white.svg'}
+              src={"/icons/Social/github-white.svg"}
               width={35}
               height={35}
-              alt={''}
+              alt={""}
             />
           </div>
-          <ul className='hidden md:flex'>
-            <Link href={'/'}>
-              <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+          <ul className="hidden md:flex">
+            <Link href={"/"}>
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
             </Link>
-            <Link href={'/about'}>
-              <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+            <Link href={"/about"}>
+              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
             </Link>
-            <Link href={'/skills'}>
-              <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
+            <Link href={"/skills"}>
+              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
             </Link>
-            <Link href={'/projects'}>
-              <li className='ml-10 text-sm uppercase hover:border-b'>
+            <Link href={"/projects"}>
+              <li className="ml-10 text-sm uppercase hover:border-b">
                 Projects
               </li>
             </Link>
@@ -71,57 +71,57 @@ const NewHeader = () => {
       </div>
       <div
         className={
-          open ? 'fixed left-0 top-0 w-full h-screen md:hidden bg-black/60' : ''
+          open ? "fixed left-0 top-0 w-full h-screen md:hidden bg-black/60" : ""
         }
       >
         <div
           className={
             open
-              ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-base-100 p-10 ease-in duration-500'
-              : 'fixed left-[-100%] top-0 w-[75%] p-10 ease-in duration-500'
+              ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-base-100 p-10 ease-in duration-500"
+              : "fixed left-[-100%] top-0 w-[75%] p-10 ease-in duration-500"
           }
         >
-          <div className=''>
-            <div className='flex w-full items-center justify-between'>
+          <div className="">
+            <div className="flex w-full items-center justify-between">
               <Image
-                src={'/BhaveshPatilText.png'}
+                src={"/BhaveshPatilText.png"}
                 width={170}
                 height={50}
-                alt={''}
+                alt={""}
               />
               <div
-                className='rounded-full shadow-lg shadow-base-300 m-4 p-2 cursor-pointer active:scale-75 ease-in-out duration-150'
+                className="rounded-full shadow-lg shadow-base-300 m-4 p-2 cursor-pointer active:scale-75 ease-in-out duration-150"
                 onClick={toggleMenu}
               >
                 <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
                   strokeWidth={1.5}
-                  stroke='currentColor'
-                  className='w-6 h-6'
+                  stroke="currentColor"
+                  className="w-6 h-6"
                 >
                   <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M6 18L18 6M6 6l12 12'
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
                   />
                 </svg>
               </div>
             </div>
-            <div className='py-4 flex flex-col'>
-              <ul className='uppercase'>
-                <Link href={'/'}>
-                  <li className='py-4 text-sm '>Home</li>
+            <div className="py-4 flex flex-col">
+              <ul className="uppercase">
+                <Link href={"/"}>
+                  <li className="py-4 text-sm ">Home</li>
                 </Link>
-                <Link href={'/about'}>
-                  <li className='py-4 text-sm '>About</li>
+                <Link href={"/about"}>
+                  <li className="py-4 text-sm ">About</li>
                 </Link>
-                <Link href={'/skills'}>
-                  <li className='py-4 text-sm '>Skills</li>
+                <Link href={"/skills"}>
+                  <li className="py-4 text-sm ">Skills</li>
                 </Link>
-                <Link href={'/projects'}>
-                  <li className='py-4 text-sm '>Projects</li>
+                <Link href={"/projects"}>
+                  <li className="py-4 text-sm ">Projects</li>
                 </Link>
                 {/* <Link href={'/'}>
                   <li className='py-4 text-sm '>Contact</li>
