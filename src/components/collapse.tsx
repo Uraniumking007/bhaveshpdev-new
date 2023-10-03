079ea98f1278a982af3283f23a48224326ee3cc6
Bhavesh Patil <96092183+Uraniumking007@users.noreply.github.com>
2023-10-03 15:12:21 +0530

    Bug Fix

--- src/components/collapse.tsx
+++ src/components/collapse.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'react';
-import { ProjectsDetails, projectsArr } from '../utils/projectArr';
-import Link from 'next/link';
+import { type ProjectsDetails, projectsArr } from "@/utils/data";
+import Link from "next/link";
 
 export const Collapse = ({
   title,
@@ -10,13 +9,13 @@ export const Collapse = ({
   return (
     <div
       tabIndex={0}
-      className=' mx-auto collapse collapse-arrow border border-base-300 bg-base-100 rounded-box w-[70%]'
+      className=" collapse collapse-arrow rounded-box mx-auto w-[70%] border border-base-300 bg-base-100"
     >
-      <div className='collapse-title text-xl font-medium'>{title}</div>
-      <div className='collapse-content'>
+      <div className="collapse-title text-xl font-medium">{title}</div>
+      <div className="collapse-content">
         <p>{description}</p>
         <Link href={githubLink}>
-          <button className='btn'>github</button>
+          <button className="btn">github</button>
         </Link>
       </div>
     </div>
@@ -25,7 +24,7 @@ export const Collapse = ({
 
 const CollapseProjects = () => {
   return (
-    <div className='flex flex-col justify-center w-full'>
+    <div className="flex w-full flex-col justify-center">
       {projectsArr.map((project, key) => {
         return (
           <div key={key}>
