3a4ae988e8b47eef14e2b20bbd07e9d54fd8cb6a
Bhavesh Patil <96092183+Uraniumking007@users.noreply.github.com>
2023-12-15 15:00:59 +0000

    Added Create Project

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -5,8 +5,41 @@ import {
   protectedProcedure,
   publicProcedure,
 } from "@/server/api/trpc";
+import { prisma } from "@/server/db";
 
 export const exampleRouter = createTRPCRouter({
+  createProject: protectedProcedure
+    .input(
+      z.object({
+        name: z.string().min(3),
+        description: z.string().min(3),
+        projectInitiated: z.date(),
+        projectCompleted: z.date(),
+        githubUrl: z.string().url(),
+        projectUrl: z.string().url(),
+        ImageUrl: z.string().url(),
+        languages: z.array(z.string()),
+      }),
+    )
+    .query(async ({ input }) => {
+      await prisma.projects.create({
+        data: {
+          name: input.name,
+          description: input.description,
+          projectInitiated: input.projectInitiated,
+          projectCompleted: input.projectCompleted,
+          image: input.ImageUrl,
+          link: input.projectUrl,
+          github: input.githubUrl,
+          tech: input.languages,
+        },
+      });
+
+      return {
+        name: input.name,
+        id: 1,
+      };
+    }),
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
