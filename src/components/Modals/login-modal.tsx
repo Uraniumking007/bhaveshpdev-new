19d8ad271a4ca4866aff3b6a52a50c1895ea04bd
Bhavesh Patil <96092183+Uraniumking007@users.noreply.github.com>
2023-12-15 14:04:32 +0000

    Added Login Functionality

---
+++ src/components/Modals/login-modal.tsx
@@ -0,0 +1,88 @@
+import {
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  Input,
+  DialogFooter,
+  Button,
+} from "@material-tailwind/react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+
+const LoginModal = () => {
+  const router = useRouter();
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+  const [openLoginModal, setOpenLoginModal] = useState<boolean>(false);
+
+  const handleLoginModal = () => setOpenLoginModal(!openLoginModal);
+
+  const handleLogin = async () => {
+    await signIn("credentials", {
+      email: loginData.email,
+      password: loginData.password,
+      callbackUrl: "/adminjod",
+      redirect: false,
+    });
+    console.log("Signed");
+
+    setOpenLoginModal(false);
+  };
+
+  return (
+    <div>
+      <Dialog handler={handleLoginModal} open={true}>
+        <DialogHeader> Login Now</DialogHeader>
+        <DialogBody>
+          <div className="flex  flex-col gap-8">
+            <Input
+              crossOrigin=""
+              variant="static"
+              label="Email"
+              type="email"
+              onChange={(e) => {
+                setLoginData({ ...loginData, email: e.target.value });
+              }}
+              placeholder="yourname@mail.com"
+            />
+            <Input
+              crossOrigin=""
+              type="password"
+              variant="static"
+              label="Password"
+              onChange={(e) => {
+                setLoginData({ ...loginData, password: e.target.value });
+              }}
+              placeholder="password"
+            />
+          </div>
+        </DialogBody>
+        <DialogFooter>
+          <div className="flex gap-4">
+            <Button
+              className="btn btn-primary"
+              onClick={() => {
+                void handleLogin();
+              }}
+            >
+              Login
+            </Button>
+            <Button
+              className="btn btn-primary"
+              onClick={() => {
+                void router.push("/");
+              }}
+            >
+              Go Home
+            </Button>
+          </div>
+        </DialogFooter>
+      </Dialog>
+    </div>
+  );
+};
+
+export default LoginModal;
--- src/pages/adminjod/index.tsx
+++ src/pages/adminjod/index.tsx
@@ -1,19 +1,66 @@
+import CreateProjectModal from "@/components/Modals/create-project";
+import LoginModal from "@/components/Modals/login-modal";
 import { getServerAuthSession } from "@/server/auth";
 import { prisma } from "@/server/db";
-import { GetServerSideProps } from "next";
-import { useSession } from "next-auth/react";
+import { Button } from "@material-tailwind/react";
+import { type GetServerSideProps } from "next";
+import { signOut, useSession } from "next-auth/react";
+import React, { useState } from "react";
 
 const AdminDashboard = () => {
-  return <div>Admin Dashboard</div>;
+  const { data: session, status } = useSession();
+  const [openProjectModal, setOpenProjectModal] = useState<boolean>(false);
+
+  console.log(session);
+
+  async function handleLogOut() {
+    await signOut();
+  }
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (status === "unauthenticated") {
+    return <LoginModal />;
+  }
+
+  return (
+    <div>
+      Admin Dashboard
+      <br />
+      <Button
+        onClick={() => {
+          void handleLogOut();
+        }}
+        className="btn btn-primary"
+      >
+        Logout
+      </Button>
+      <br />
+      <Button
+        onClick={() => {
+          setOpenProjectModal(true);
+        }}
+        className="btn btn-primary"
+      >
+        Create Project
+      </Button>
+      <CreateProjectModal
+        openProjectModal={openProjectModal}
+        setOpenProjectModal={setOpenProjectModal}
+      />
+    </div>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const session = await getServerAuthSession(ctx);
   if (!session?.user.email) {
     return {
-      redirect: {
-        destination: "/adminjod/login",
-        permanent: false,
+      props: {
+        isAllowed: false,
+        user: null,
       },
     };
   }
@@ -24,9 +71,9 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     },
   });
 
-  
-
   return {
-    props: { session },
+    props: { isAllowed: user?.isAdmin ? true : false, user },
   };
 };
+
+export default AdminDashboard;
