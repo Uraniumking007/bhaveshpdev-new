98da860507c30be998baa7b1164b84aa13bd27dc
UraniumKing007 <bhaveshpatil918@gmail.com>
2024-04-21 13:28:53 +0530

    Refactor ProjectPage component to use Prisma client for fetching project data

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,22 +1,25 @@
 import ProjectCard from "@/components/cards/project-card";
 import { HeroHighlight } from "@/components/hero-highlight";
 import { Meteors } from "@/components/meteors";
+import { prisma } from "@/lib/prisma";
 import Image from "next/image";
 import React from "react";
 
-const ProjectPage: React.FC = () => {
+const ProjectPage: React.FC = async () => {
+  const projects = await getData();
   return (
-    <HeroHighlight>
-      <div className="flex flex-wrap gap-8 w-full justify-center">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+    <HeroHighlight className="">
+      <div className="relative top-16 left-10 flex flex-wrap gap-12 w-[95%] py-10 justify-center h-fit overflow-visible">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} {...project} />
+        ))}
       </div>
     </HeroHighlight>
   );
 };
 
 export default ProjectPage;
+
+const getData = async () => {
+  return await prisma.projects.findMany();
+};
