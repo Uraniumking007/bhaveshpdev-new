bf08dbcaae62dbd8bd3b607ff5b5e29a82b119b5
UraniumKing007 <bhaveshpatil918@gmail.com>
2024-04-21 13:55:20 +0530

    Fix formatting issues in Tabs component and update CSS styles

--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -40,7 +40,7 @@ export const Tabs = ({
     <>
       <div
         className={cn(
-          "flex flex-row items-center justify-start [perspective:1000px] relative overflow-auto sm:overflow-visible no-visible-scrollbar max-w-full w-full",
+          "flex flex-row items-center justify-start [perspective:1000px] relative top-7 left-24 overflow-auto sm:overflow-visible no-visible-scrollbar max-w-full w-full",
           containerClassName
         )}
       >
@@ -79,7 +79,7 @@ export const Tabs = ({
         active={active}
         key={active.value}
         hovering={hovering}
-        className={cn("mt-32", contentClassName)}
+        className={cn("mt-8", contentClassName)}
       />
     </>
   );
