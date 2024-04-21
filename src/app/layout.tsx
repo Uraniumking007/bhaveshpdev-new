a5a7e9b4e778b76eecc07760af19ff81bd6886e0
UraniumKing007 <bhaveshpatil918@gmail.com>
2024-04-21 12:35:59 +0530

    Update classNames in layout.tsx and page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} w-screen h-screen dark`}>
         <NavBar />
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import DevInfo from "@/components/dev-info";
 
 export default function Home() {
-  return (
-    <div className="w-screen h-screen dark">
-      <DevInfo />
-    </div>
-  );
+  return <DevInfo />;
 }
