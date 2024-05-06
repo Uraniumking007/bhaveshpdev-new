4b68eded9e72d59f60b4f89b7fc520e9e4775ca7
UraniumKing007 <bhaveshpatil918@gmail.com>
2024-05-06 23:32:11 +0530

    chore: Add lazy loading for images in ProjectPage component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,16 +1,19 @@
 import ProjectCard from "@/components/cards/project-card";
 import { HeroHighlight } from "@/components/hero-highlight";
+import { Loading } from "@/components/loading/default-loading";
 import { prisma } from "@/lib/prisma";
-import React from "react";
+import React, { Suspense } from "react";
 
 const ProjectPage: React.FC = async () => {
   const projects = await getProjects();
   return (
     <HeroHighlight className="">
       <div className="relative top-16 left-10 flex flex-wrap gap-12 w-[95%] py-10 justify-center h-fit overflow-visible">
-        {projects.map((project) => (
-          <ProjectCard key={project.id} {...project} />
-        ))}
+        <Suspense fallback={<Loading />}>
+          {projects.map((project) => (
+            <ProjectCard key={project.id} {...project} />
+          ))}
+        </Suspense>
       </div>
     </HeroHighlight>
   );
@@ -19,5 +22,9 @@ const ProjectPage: React.FC = async () => {
 export default ProjectPage;
 
 const getProjects = async () => {
-  return await prisma.projects.findMany();
+  return await prisma.projects.findMany({
+    orderBy: {
+      projectCompleted: "desc",
+    },
+  });
 };
