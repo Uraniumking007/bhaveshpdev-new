474d902bcb9aea8023dbb370305e6871e95514c3
UraniumKing <bhaveshpatil918@gmail.com>
2024-05-08 02:09:37 +0530

    feat: Create SkillsIcons component for displaying skills icons

---
+++ src/components/tabs/skills-icons.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import SkillIcons from "../Buttons/magic-border-button";
+
+export default function SkillsIcons() {
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <SkillIcons />
+    </div>
+  );
+}
