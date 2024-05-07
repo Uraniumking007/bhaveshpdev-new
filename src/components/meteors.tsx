93c7d13e1550ddc09c5baff670057dee0ee5981e
UraniumKing <bhaveshpatil918@gmail.com>
2024-05-08 02:09:10 +0530

    refactor: Update Meteors component to render only 1 meteor by default

--- src/components/meteors.tsx
+++ src/components/meteors.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { cn } from "@/utils/cn";
-import clsx from "clsx";
 import React from "react";
 
 export const Meteors = ({
@@ -10,12 +9,12 @@ export const Meteors = ({
   number?: number;
   className?: string;
 }) => {
-  const meteors = new Array(number || 20).fill(true);
+  const meteors = new Array(number || 1).fill(true);
   return (
     <>
       {meteors.map((el, idx) => (
         <span
-          key={"meteor" + idx}
+          key={`meteor${idx}`}
           className={cn(
             "animate-meteor-effect absolute top-1/2 left-1/2 h-0.5 w-0.5 rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10] rotate-[215deg]",
             "before:content-[''] before:absolute before:top-1/2 before:transform before:-translate-y-[50%] before:w-[50px] before:h-[1px] before:bg-gradient-to-r before:from-[#64748b] before:to-transparent",
