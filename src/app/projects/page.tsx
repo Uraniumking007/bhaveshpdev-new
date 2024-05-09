1699d8e03065a47bfe1dd67835ac72704cf949b1
UraniumKing <bhaveshpatil918@gmail.com>
2024-05-09 22:31:21 +0530

    refactor: Update page layout in ProjectPage component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -15,7 +15,7 @@ const ProjectPage: React.FC = async () => {
   const projects = await getProjects();
   return (
     <HeroHighlight>
-      <div className="relative top-16 left-10 flex flex-wrap gap-12 w-[95%] py-10 justify-center h-fit overflow-visible">
+      <div className="relative top-16 md:left-10 flex flex-wrap gap-12 w-full md:w-[95%] py-10 justify-center h-fit overflow-visible">
         <Suspense fallback={<Loading />}>
           {projects.map((project) => (
             <ProjectCard key={project.id} {...project} />
