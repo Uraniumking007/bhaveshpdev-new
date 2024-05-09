1285301cd87ad8f2194196825ab72d21ebc45db3
UraniumKing <bhaveshpatil918@gmail.com>
2024-05-09 22:14:52 +0530

    refactor: Update metadata for page components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/navbar";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  icons: ["/favicon.ico"],
+};
 
 export default function RootLayout({
   children,
