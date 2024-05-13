03039ba698e0a5391a4c33680c442dc2e1395b06
UraniumKing <bhaveshpatil918@gmail.com>
2024-05-13 16:01:20 +0530

    feat: Add AdminPage component for admin dashboard
    This commit adds the AdminPage component to the admin page file. The AdminPage component is responsible for rendering the admin dashboard, including the project cards, sign-out button, and create project modal. It also includes authentication logic to ensure only admin users can access the dashboard. This feature enhances the functionality and user experience of the admin section.

---
+++ src/app/(admin)/admin/page.tsx
@@ -0,0 +1,44 @@
+import { auth } from "@/app/api/auth/[...nextauth]/auth";
+import React from "react";
+import ProjectCardEditable from "./_components/admin-project-card";
+import { prisma } from "@/lib/prisma";
+import SignOutButton from "./_components/sign-out-button";
+import CreateProjectModal from "./_components/create-project-card";
+
+export default async function AdminPage() {
+  const session = await auth();
+
+  if (!session) {
+    return <div>loading...</div>;
+  }
+
+  if (!session.user.isAdmin) {
+    return <div>forbidden</div>;
+  }
+
+  const projects = await prisma.projects.findMany({
+    orderBy: {
+      projectInitiated: "desc",
+    },
+  });
+
+  return (
+    <div className="h-screen w-full items-center flex flex-col">
+      <div className="pt-16 text-center">
+        <h1>Admin Dashboard</h1>
+        <p>Welcome {session.user.username}</p>
+        <div className="w-full flex gap-10">
+          <SignOutButton />
+          <CreateProjectModal />
+        </div>
+      </div>
+      <div className="pt-8 flex justify-center items-center flex-wrap w-full gap-8">
+        {projects.map((project) => (
+          <div key={project.id}>
+            <ProjectCardEditable {...project} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
