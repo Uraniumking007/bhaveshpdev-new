b8ecab722694f88d7b8f38be6d93f1c6c71d6508
UraniumKing <bhaveshpatil918@gmail.com>
2024-05-13 16:03:25 +0530

    refactor: Optimize ProjectCard component and add preview and GitHub links
    This commit optimizes the ProjectCard component by removing unused imports and improving the code structure. It also adds preview and GitHub links to the project card, allowing users to easily access the live preview and GitHub repository of the project. This enhancement improves the functionality and user experience of the project card.

--- src/components/cards/project-card.tsx
+++ src/components/cards/project-card.tsx
@@ -4,6 +4,12 @@ import { Meteors } from "../meteors";
 import { Projects } from "@prisma/client";
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
+import {
+  IconBrandGithub,
+  IconBrowser,
+  IconGlobe,
+  IconWorld,
+} from "@tabler/icons-react";
 
 export default function ProjectCard(project: Projects) {
   console.log(project.image);
@@ -68,16 +74,31 @@ export default function ProjectCard(project: Projects) {
           <Link
             href={project.link}
             target="_blank"
-            className="inline-flex h-8 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+            className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block"
           >
-            Preview
+            <span className="absolute inset-0 overflow-hidden rounded-full">
+              <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+            </span>
+            <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+              <IconWorld size={15} />
+              <span>{`Preview`}</span>
+            </div>
+            <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
           </Link>
+
           <Link
-            target="_blank"
             href={project.github}
-            className="inline-flex h-8 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+            target="_blank"
+            className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block"
           >
-            Github
+            <span className="absolute inset-0 overflow-hidden rounded-full">
+              <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+            </span>
+            <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+              <IconBrandGithub size={15} />
+              <span>{`Github`}</span>
+            </div>
+            <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
           </Link>
         </div>
 
