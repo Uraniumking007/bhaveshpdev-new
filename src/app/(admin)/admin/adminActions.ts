aecf3f73b619113014ffc18ff956ae097357d163
UraniumKing <bhaveshpatil918@gmail.com>
2024-05-13 20:21:53 +0530

    feat: Add isDemo field to User model
    This commit adds the isDemo field to the User model in the prisma schema. The isDemo field is a boolean that indicates whether a user is a demo user. This addition allows for better user management and differentiation between regular users and demo users.

--- src/app/(admin)/admin/adminActions.ts
+++ src/app/(admin)/admin/adminActions.ts
@@ -1,8 +1,24 @@
 "use server";
+import { auth } from "@/app/api/auth/[...nextauth]/auth";
 import { prisma } from "@/lib/prisma";
 import { Projects } from "@prisma/client";
 
 export async function createProject({ project }: { project: Projects }) {
+  const user = await auth();
+  if (!user) {
+    throw new Error("Unauthorized");
+  }
+
+  const userData = await prisma.user.findFirst({
+    where: {
+      email: user.user.email,
+    },
+  });
+
+  if (!userData?.isAdmin) {
+    throw new Error("Unauthorized");
+  }
+
   return await prisma.projects.create({
     data: {
       id: crypto.randomUUID(),
@@ -21,6 +37,22 @@ export async function createProject({ project }: { project: Projects }) {
 }
 
 export async function editProject({ project }: { project: Projects }) {
+  const user = await auth();
+
+  if (!user) {
+    throw new Error("Unauthorized");
+  }
+
+  const userData = await prisma.user.findFirst({
+    where: {
+      email: user.user.email,
+    },
+  });
+
+  if (!userData?.isAdmin) {
+    throw new Error("Unauthorized");
+  }
+
   await prisma.projects.update({
     data: {
       name: project.name,
