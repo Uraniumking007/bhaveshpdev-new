f70d6c98364158dac10143f4f8329fcbcc4d68d7
UraniumKing007 <bhaveshpatil918@gmailcom>
2024-08-07 20:26:26 +0530

    refactor: Update NextAuth configuration and providers

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,9 @@
-import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
-import { User } from "next-auth";
+import { DefaultSession } from "next-auth";
 import { DefaultJWT } from "next-auth/jwt";
-import { authOptions } from "./auth";
+import { handlers } from "./auth";
 
 declare module "next-auth" {
-  interface User extends DefaultUser {
-    id: string;
-    email: string;
+  interface User {
     isAdmin: boolean;
     isDemo: boolean;
     username: string;
@@ -27,17 +24,17 @@ declare module "next-auth" {
       username: string;
     };
   }
-}
-
-declare module "next-auth/adapters" {
-  interface AdapterUser extends User {
+  interface AdapterUser {
     id: string;
     email: string;
     isAdmin: boolean;
     isDemo: boolean;
+    username: string;
   }
 }
 
-const handler = NextAuth(authOptions);
+// const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
+
+export const { GET, POST } = handlers;
