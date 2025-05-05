d1527c08cadecf50983316475cfcf2b2f8ae4b24
Bhavesh Patil <bhaveshpatil918@gmail.com>
2025-05-05 11:23:28 +0530

    refactor: Add revalidateAdminPages function to refresh admin and project paths

--- src/app/(admin)/admin/adminActions.ts
+++ src/app/(admin)/admin/adminActions.ts
@@ -2,6 +2,7 @@
 import { auth } from "@/app/api/auth/[...nextauth]/auth";
 import { prisma } from "@/lib/prisma";
 import { Projects } from "@prisma/client";
+import { revalidatePath } from "next/cache";
 
 export async function createProject({ project }: { project: Projects }) {
   const user = await auth();
@@ -72,4 +73,10 @@ export async function editProject({ project }: { project: Projects }) {
       id: project.id,
     },
   });
+}
+
+export async function revalidateAdminPages() {
+  "use server";
+  revalidatePath("/admin");
+  revalidatePath("/projects");
 }
