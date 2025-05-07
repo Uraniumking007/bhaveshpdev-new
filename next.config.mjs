4e36123cc67aa06e03681a57ecb1969ed7ec0d78
Bhavesh Patil <bhaveshpatil918@gmail.com>
2025-05-07 18:06:45 +0530

    feat: add new HTTPS configuration for i.vgy.me in Next.js settings

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "i.vgy.me",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
