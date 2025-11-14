dbeffac02ed09ad6e70cf3f3af7c83701df836a5
UraniumKing007 <bhaveshpatil918@gmail.com>
2025-11-14 13:21:22 +0530

    refactor: simplify canvasRef initialization in StarsBackground component

--- src/components/ui/stars-background.tsx
+++ src/components/ui/stars-background.tsx
@@ -34,7 +34,7 @@ export const StarsBackground: React.FC<StarBackgroundProps> = ({
   className,
 }) => {
   const [stars, setStars] = useState<StarProps[]>([]);
-  const canvasRef: RefObject<HTMLCanvasElement> =
+  const canvasRef =
     useRef<HTMLCanvasElement>(null);
 
   const generateStars = useCallback(
