c6fee30cb78e1a49d6f5dbbc9989f05d21b1f0b9
UraniumKing007 <bhaveshpatil918@gmail.com>
2025-12-10 15:55:29 +0530

    feat: include technologies and categories in project retrieval for enhanced project details

--- src/app/api/v1/projects/route.ts
+++ src/app/api/v1/projects/route.ts
@@ -3,6 +3,18 @@ import { prisma } from "@/lib/prisma";
 
 export async function GET() {
   const data = await prisma.projects.findMany({
+    include: {
+      technologies: {
+        include: {
+          technology: true,
+        },
+      },
+      projectCategories: {
+        include: {
+          category: true,
+        },
+      },
+    },
     orderBy: {
       projectCompleted: "desc",
     },
