b6bcfd40297d27ddc9aaff32eb155095b3cb09cb
UraniumKing007 <bhaveshpatil918@gmail.com>
2026-02-01 14:43:45 +0530

    feat: add data layer for static content
    - Add data files for certifications, projects, and skills
    - Add social media and contact information data
    - Add profile and timeline data

---
+++ src/lib/data/skills.ts
@@ -0,0 +1,89 @@
+import type { Skill } from "@/types/static-data";
+
+export const skills: Skill[] = [
+  {
+    language: "HTML",
+    src: "/brand-icons/FrontendDevelopment/html.svg",
+    colorScheme: "rgba(251,110,34,0.5)",
+  },
+  {
+    language: "CSS",
+    src: "/brand-icons/FrontendDevelopment/css.svg",
+    colorScheme: "rgba(38,154,227,0.5)",
+  },
+  {
+    language: "TailwindCSS",
+    src: "/brand-icons/FrontendDevelopment/tailwindcss.svg",
+    colorScheme: "rgba(56,189,248,0.5)",
+  },
+  {
+    language: "React.js",
+    src: "/brand-icons/FrontendDevelopment/reactjs.svg",
+    colorScheme: "rgba(0, 216, 255,0.5)",
+  },
+  {
+    language: "Next.js",
+    src: "/brand-icons/Framework/nextjs.ico",
+    colorScheme: "rgba(0, 0, 0,0.5)",
+  },
+  {
+    language: "Xata",
+    src: "/brand-icons/Database/xatafly.png",
+    colorScheme: "rgba(254, 1, 116,0.5)",
+  },
+  {
+    language: "Supabase",
+    src: "/brand-icons/Database/supabase.png",
+    colorScheme: "rgba(64, 207, 143,0.5)",
+  },
+  {
+    language: "GitHub",
+    src: "/brand-icons/Social/github.svg",
+    colorScheme: "rgba(92, 107, 192,0.5)",
+  },
+  {
+    language: "JavaScript",
+    src: "/brand-icons/ProgrammingLanguages/javascript.svg",
+    colorScheme: "rgba(255, 214, 0,0.5)",
+  },
+  {
+    language: "Oracle",
+    src: "/brand-icons/Database/oracle.svg",
+    colorScheme: "rgba(199, 70, 52,0.5)",
+  },
+  {
+    language: "MongoDB",
+    src: "/brand-icons/Database/mongodb.svg",
+    colorScheme: "rgba(70, 160, 55,0.5)",
+  },
+  {
+    language: "MySQL",
+    src: "/brand-icons/Database/mysql.svg",
+    colorScheme: "rgba(4, 77, 99,0.5)",
+  },
+  {
+    language: "Node.js",
+    src: "/brand-icons/BackendDevelopment/nodejs.svg",
+    colorScheme: "rgba(83, 158, 67,0.5)",
+  },
+  {
+    language: "Express.js",
+    src: "/brand-icons/BackendDevelopment/express.svg",
+    colorScheme: "rgba(0, 0, 0,0.5)",
+  },
+  {
+    language: "Photoshop",
+    src: "/brand-icons/Software/photoshop.svg",
+    colorScheme: "rgba(49, 168, 255,0.5)",
+  },
+  {
+    language: "Postman",
+    src: "/brand-icons/Software/postman.svg",
+    colorScheme: "rgba(255, 108, 55,0.5)",
+  },
+  {
+    language: "C",
+    src: "/brand-icons/ProgrammingLanguages/c.svg",
+    colorScheme: "rgba(57, 73, 171,0.5)",
+  },
+];
