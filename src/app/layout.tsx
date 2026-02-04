af0853c084a6a58afa62b0c47195460a4cdb8f09
UraniumKing007 <bhaveshpatil918@gmail.com>
2026-02-04 10:41:55 +0530

    chore: update dependencies and add analytics script
    Updated several packages including:
    - @internationalized/date, @prisma/client
    - @react-pdf/renderer, @tabler/icons
    - framer-motion, gsap, lightningcss
    - nodemailer, pdfjs-dist, react, react-dom
    - react-hook-form, react-pdf, tailwindcss, zod
    - Updated dev dependencies and added a script for analytics integration in the layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Metadata } from "next";
+import Script from "next/script";
 import PageTransitionWrapper from "@/components/wrapper/PageTransitionWrapper";
 import { TransitionProvider } from "@/lib/context/TransitionContext";
 
@@ -20,6 +21,11 @@ export default function RootLayout({
       <body
         className={`${inter.className} w-full min-h-screen overflow-x-hidden dark`}
       >
+        <Script
+          src="https://analytics.bhaveshp.dev/script.js"
+          data-website-id="711e7bc2-cee7-4f07-bae4-f8cb22c643f4"
+          strategy="afterInteractive"
+        />
         <TransitionProvider>
           <PageTransitionWrapper>{children}</PageTransitionWrapper>
         </TransitionProvider>
